48305dffb5eff15f64b20f9c957b1110610602f8
b <ben.r.stadnick@gmail.com>
2021-05-08 18:56:24 -0400

    Update geocode.js

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const geocode = (address, callback) => {
-	const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoiYi1zdGFkIiwiYSI6ImNraWk2ZnlsZTAwaXQydnA5OTNxZHhiNjkifQ.ixA4T3zpI5qqZS2JBOUhCg&limit=1'
+	const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=&limit=1'
 	
 	request({url, json: true}, (error, {body}) => {
 		if (error) {
